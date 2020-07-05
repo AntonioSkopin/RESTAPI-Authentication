e8e94b181ecf96e33a1c61c0f3c8062376452f70
AntonioSkopin <askopin@outlook.com>
2020-07-05 13:53:35 +0200

    Created a JWS token and check if user is allowed to login

--- Backend/api/login.php
+++ Backend/api/login.php
@@ -24,3 +24,46 @@
     // Set product property values
     $user->username = $data->username;
     $username_exists = $user->usernameExists();
+
+    // generate json web token
+    include_once 'config/core.php';
+    include_once 'libs/php-jwt-master/src/BeforeValidException.php';
+    include_once 'libs/php-jwt-master/src/ExpiredException.php';
+    include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
+    include_once 'libs/php-jwt-master/src/JWT.php';
+    use \Firebase\JWT\JWT;
+
+    // Check if username exists and if password is correct
+    if ($username_exists && password_verify($data->password, $user->password)) {
+        $token = array(
+            "iss" => $iss,
+            "aud" => $aud,
+            "iat" => $iat,
+            "nbf" => $nbf,
+            "data" => array(
+                "id" => $user->id,
+                "username" => $user->username,
+                "email" => $user->email
+            )
+        );
+
+        // Set response code to 200 (OK)
+        http_response_code(200);
+
+        // Generate JWT
+        $jwt = JWT::encode($token, $key);
+
+        // Tell user login was succesful
+        echo json_encode(array(
+            "message" => "Succesful login.",
+            "jwt" => $jwt
+        ));
+    }
+    // Login failed
+    else {
+        // Set response code to 401 (Unauthorized client)
+        http_response_code(401);
+
+        // Tell the user login failed
+        echo json_encode(array("message" => "Login failed."));
+    }
