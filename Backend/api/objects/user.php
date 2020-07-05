b3dea27d0ebfcb42037d1b0dbca4be46e17a5f27
AntonioSkopin <askopin@outlook.com>
2020-07-05 13:10:44 +0200

    Added method to check if the username or email is taken

--- Backend/api/objects/user.php
+++ Backend/api/objects/user.php
@@ -17,6 +17,7 @@
             $this->conn = $db;
         }
 
+        // Method to create an user
         public function create() {
             // Insert query
             $query = "INSERT INTO ".$this->table_name." 
@@ -40,6 +41,33 @@
 
             // Execute query & check if query was succesfull
             if ($stmt->execute()) {
+                return true;
+            }
+            return false;
+        }
+
+        // Method to check if the username or email is taken
+        function isTaken() {
+            // Select username and email query
+            $query = "SELECT username, email FROM " .$this->table_name. " 
+            WHERE username=:username OR email=:email";
+
+            // Prepare the query
+            $stmt = $this->conn->prepare($query);
+
+            // Sanitize
+            $this->username = htmlspecialchars(strip_tags($this->username));
+            $this->email = htmlspecialchars(strip_tags($this->email));
+
+            // Bind valuess
+            $stmt->bindParam("username", $this->username);
+            $stmt->bindParam("email", $this->email);
+
+            // Execute query
+            $stmt->execute();
+
+            // Check if username or email is taken
+            if ($stmt->rowCount() > 0) {
                 return true;
             }
             return false;
