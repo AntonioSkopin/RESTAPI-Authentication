b07d11ccf5a2b345baf5edb694512d1c91d3d7a3
AntonioSkopin <askopin@outlook.com>
2020-07-05 13:03:10 +0200

    Added create method to insert a user in the database

--- Backend/api/objects/user.php
+++ Backend/api/objects/user.php
@@ -16,4 +16,32 @@
         public function __construct($db) {
             $this->conn = $db;
         }
+
+        public function create() {
+            // Insert query
+            $query = "INSERT INTO ".$this->table_name." 
+            SET username=:username, email=:email, password=:password";
+
+            // Prepare the query
+            $stmt = $this->conn->prepare($query);
+
+            // Sanitize
+            $this->username = htmlspecialchars(strip_tags($this->username));
+            $this->email = htmlspecialchars(strip_tags($this->email));
+            $this->password = htmlspecialchars(strip_tags($this->password));
+
+            // Bind valuess
+            $stmt->bindParam("username", $this->username);
+            $stmt->bindParam("email", $this->email);
+
+            // Hash the password before saving it to the database
+            $password_hashed = password_hash($this->password, PASSWORD_BCRYPT);
+            $stmt->bindParam("password", $password_hashed);
+
+            // Execute query & check if query was succesfull
+            if ($stmt->execute()) {
+                return true;
+            }
+            return false;
+        }
     }
